a7df4e7f1e98ed65e072eafccb8c934823370861
stephst10 <stephaniesst10@gmail.com>
2024-09-18 11:01:51 -0300

    square

--- script.js
+++ script.js
@@ -32,3 +32,16 @@ function updateCirclePosition() {
     circle.style.left = `${posX}px`;
     circle.style.top = `${posY}px`;
 }
+
+function createSquares() {
+    clearShapes();
+    gameArea.style.backgroundColor = 'red';
+
+    for (let i = 0; i < 4; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.left = `${Math.random() * (window.innerWidth - 50)}px`;
+        square.style.top = `${Math.random() * (window.innerHeight - 50)}px`;
+        gameArea.appendChild(square);
+    }
+}
