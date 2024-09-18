1abdd0488db39916dac2a2f10a6324053732a208
stephst10 <stephaniesst10@gmail.com>
2024-09-18 10:32:04 -0300

    update circle position

---
+++ script.js
@@ -0,0 +1,34 @@
+const circle = document.getElementById('circle');
+const gameArea = document.getElementById('gameArea');
+
+let posX = 50;
+let posY = 50;
+
+const moveSpeed = 10;
+const originalBackgroundColor = '#7fffd4';
+
+document.addEventListener('keydown', (event) => {
+    switch (event.code) {
+        case 'ArrowUp': 
+            if (posY > 0) posY -= moveSpeed;
+            break;
+
+        case 'ArrowDown': 
+            if (posY < window.innerHeight - circle.offsetHeight) posY += moveSpeed;
+            break;
+
+        case 'ArrowLeft': 
+            if (posX > 0) posX -= moveSpeed;
+            break; 
+
+        case 'ArrowRight':
+            if (posX < window.innerWidth - circle.offsetWidth) posX += moveSpeed;
+            break;
+    }
+    updateCirclePosition();
+});
+
+function updateCirclePosition() {
+    circle.style.left = `${posX}px`;
+    circle.style.top = `${posY}px`;
+}
